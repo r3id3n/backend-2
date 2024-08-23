5f5c9d83ce319bde11b5941cb64612cb0a31f503
Felipe Valencia <r3id3n2091@hotmail.com>
2024-08-22 21:32:52 -0400

    PRE-ENTREGA N1 - style

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 8080;
 import "./database.js";
 
+app.use(express.static('src/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
